6943f85c99f2680a0fb7b92dffbad7982e809813
WithoutLogic <51289664+WithoutLogic@users.noreply.github.com>
2019-10-28 12:46:01 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@ client.on('message', async message => {
 	//if(message.author.bot) return;
 	
 	if (message.content === '!ping') {
-		const m = await message.channel.send("Ping?");
-		message.reply(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+		const m = await message.channel.send("testing...");
+		message.reply(`Pong! Timestamp latency is ${m.createdTimestamp - message.createdTimestamp}ms. Discord API Latency is ${Math.round(client.ping)}ms`);
 	}
 });
 
